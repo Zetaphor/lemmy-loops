ff692c7c58d91528ab904d7754177d4e27cafe03
Zetaphor <zetaphor@hey.com>
2023-06-28 18:18:03 -0300

    Add proxy to resolve cors issue

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,34 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    cors: false,
+    proxy: {
+      '/api': {
+        target: 'https://zemmy.cc/api',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+        configure: (proxy, _options) => {
+          proxy.on('error', (err, _req, _res) => {
+            console.log('proxy error', err)
+          })
+          proxy.on('proxyReq', (proxyReq, req, _res) => {
+            proxyReq.removeHeader('cookie')
+            proxyReq.setHeader('origin', `https://zemmy.cc`)
+            // console.log('Sending Request to the Target:', req.method, req.url)
+          })
+          proxy.on('proxyRes', (proxyRes, req, _res) => {
+            // console.log('Received Response from the Target:', proxyRes.statusCode, req.url)
+          })
+        }
+      }
     }
   }
 })
