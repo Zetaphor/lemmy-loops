619c3fcd367c685ce1ddb7da2936871e380aabf9
Zetaphor <zetaphor@hey.com>
2023-06-29 12:49:23 -0300

    Move app script

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,30 @@
+<template>
+  <div ref="appContainer">
+    <DarkOverlay />
+    <LeftSidebar />
+    <RightSidebar />
+    <header class="absolute top-0 w-full">
+      <TopNav />
+    </header>
+    <main class="pt-20 pb-20">
+      <RouterView />
+    </main>
+    <footer>
+      <BottomNav />
+    </footer>
+  </div>
+</template>
+
 <script setup>
+import { ref } from 'vue'
 import { RouterView } from 'vue-router'
 import TopNav from './components/TopNav.vue';
 import BottomNav from './components/BottomNav.vue';
 import LeftSidebar from './components/LeftSidebar.vue';
-import ModalAddAccount from './components/ModalAddAccount.vue';
 
-// import { useScreenStore } from '@/stores/screen'
 import DarkOverlay from './components/DarkOverlay.vue';
 import RightSidebar from './components/RightSidebar.vue';
 
-// const screen = useScreenStore()
+const appContainer = ref(null)
+
 </script>
-
-<template>
-  <DarkOverlay />
-  <ModalAddAccount />
-  <LeftSidebar />
-  <RightSidebar />
-  <header class="absolute top-0 w-full">
-    <TopNav />
-  </header>
-  <main class="pt-20 pb-20">
-    <RouterView />
-  </main>
-  <footer>
-    <BottomNav />
-  </footer>
-</template>
-
-<style scoped></style>
