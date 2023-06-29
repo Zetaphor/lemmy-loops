cd778c469cb4075acdaad87c4a510180663a1780
Zetaphor <zetaphor@hey.com>
2023-06-29 09:29:22 -0300

    Add markdown component

---
+++ src/components/Markdown.vue
@@ -0,0 +1,133 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div v-html="html"></div>
+</template>
+
+<script setup>
+import MarkdownIt from 'markdown-it'
+import MarkdownItContainer from 'markdown-it-container';
+import MarkdownItEmoji from 'markdown-it-emoji';
+import MarkdownItFootnote from 'markdown-it-footnote';
+import MarkdownItHtml5Embed from 'markdown-it-html5-embed';
+import MarkdownItSub from 'markdown-it-sub';
+import MarkdownItSup from 'markdown-it-sup';
+
+const props = defineProps(['source'])
+
+const markdownItConfig = {
+  html: false,
+  linkify: true,
+  typographer: true,
+};
+
+const html5EmbedConfig = {
+  html5embed: {
+    useImageSyntax: true, // Enables video/audio embed with ![]() syntax (default)
+    attributes: {
+      audio: 'controls preload="metadata"',
+      video: 'width="100%" max-height="100%" controls loop preload="metadata"',
+    },
+  },
+};
+
+const spoilerConfig = {
+  validate: (params) => {
+    return params.trim().match(/^spoiler\s+(.*)$/);
+  },
+
+  render: (tokens, idx) => {
+    var m = tokens[idx].info.trim().match(/^spoiler\s+(.*)$/);
+
+    if (tokens[idx].nesting === 1) {
+      // opening tag
+      return `<details><summary> ${md.utils.escapeHtml(m[1])} </summary>\n`;
+    } else {
+      // closing tag
+      return "</details>\n";
+    }
+  },
+};
+
+const instanceLinkRegex =
+  /(\/[cmu]\/|!)[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/g;
+
+function localInstanceLinkParser(md) {
+  md.core.ruler.push("replace-text", state => {
+    for (let i = 0; i < state.tokens.length; i++) {
+      if (state.tokens[i].type !== "inline") {
+        continue;
+      }
+      const inlineTokens = state.tokens[i].children || [];
+      for (let j = inlineTokens.length - 1; j >= 0; j--) {
+        if (
+          inlineTokens[j].type === "text" &&
+          new RegExp(instanceLinkRegex).test(inlineTokens[j].content)
+        ) {
+          const text = inlineTokens[j].content;
+          const matches = Array.from(text.matchAll(instanceLinkRegex));
+
+          let lastIndex = 0;
+          const newTokens = [];
+
+          let linkClass = "community-link";
+
+          for (const match of matches) {
+            // If there is plain text before the match, add it as a separate token
+            if (match.index !== undefined && match.index > lastIndex) {
+              const textToken = new state.Token("text", "", 0);
+              textToken.content = text.slice(lastIndex, match.index);
+              newTokens.push(textToken);
+            }
+
+            let href;
+            if (match[0].startsWith("!")) {
+              href = "/c/" + match[0].substring(1);
+            } else if (match[0].startsWith("/m/")) {
+              href = "/c/" + match[0].substring(3);
+            } else {
+              href = match[0];
+              if (match[0].startsWith("/u/")) {
+                linkClass = "user-link";
+              }
+            }
+
+            const linkOpenToken = new state.Token("link_open", "a", 1);
+            linkOpenToken.attrs = [
+              ["href", href],
+              ["class", linkClass],
+            ];
+            const textToken = new state.Token("text", "", 0);
+            textToken.content = match[0];
+            const linkCloseToken = new state.Token("link_close", "a", -1);
+
+            newTokens.push(linkOpenToken, textToken, linkCloseToken);
+
+            lastIndex =
+              (match.index !== undefined ? match.index : 0) + match[0].length;
+          }
+
+          // If there is plain text after the last match, add it as a separate token
+          if (lastIndex < text.length) {
+            const textToken = new state.Token("text", "", 0);
+            textToken.content = text.slice(lastIndex);
+            newTokens.push(textToken);
+          }
+
+          inlineTokens.splice(j, 1, ...newTokens);
+        }
+      }
+    }
+  });
+}
+
+const markdownIt = new MarkdownIt(markdownItConfig)
+  .use(MarkdownItSub)
+  .use(MarkdownItSup)
+  .use(MarkdownItFootnote)
+  .use(MarkdownItHtml5Embed, html5EmbedConfig)
+  .use(MarkdownItContainer, "spoiler", spoilerConfig)
+  .use(MarkdownItEmoji) // TODO: Not sure if I need this? Not used in lemmy-ui
+  .use(localInstanceLinkParser);
+
+const html = markdownIt.render(props.source);
+</script>
