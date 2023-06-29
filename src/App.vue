68ce1ed19444dd87a932b45e5a0fa2490687fd06
Zetaphor <zetaphor@hey.com>
2023-06-29 18:06:24 -0300

    Set window size

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <template>
-  <div ref="appContainer">
-    <DarkOverlay />
-    <LeftSidebar />
-    <RightSidebar />
-    <header class="absolute top-0 w-full">
-      <TopNav />
-    </header>
-    <main class="pt-20 pb-20">
-      <RouterView />
-    </main>
-    <footer>
-      <BottomNav />
-    </footer>
-  </div>
+  <DarkOverlay />
+  <ToastDisplay />
+  <LeftSidebar />
+  <RightSidebar />
+  <header class="absolute top-0 w-full">
+    <TopNav />
+  </header>
+  <main class="pt-20 pb-20">
+    <RouterView />
+  </main>
+  <footer>
+    <BottomNav />
+  </footer>
 </template>
 
+<style>
+body,
+#app {
+  min-width: 100vw;
+  min-height: 100vh;
+}
+</style>
+
 <script setup>
-import { ref } from 'vue'
 import { RouterView } from 'vue-router'
 import TopNav from './components/TopNav.vue';
 import BottomNav from './components/BottomNav.vue';
 import LeftSidebar from './components/LeftSidebar.vue';
-
 import DarkOverlay from './components/DarkOverlay.vue';
 import RightSidebar from './components/RightSidebar.vue';
-
-const appContainer = ref(null)
+import ToastDisplay from './components/ToastDisplay.vue';
 
 </script>
