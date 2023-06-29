505dca2d158eddf164d864eff52e789ef6237e4f
Zetaphor <zetaphor@hey.com>
2023-06-29 09:30:35 -0300

    Add right sidebar

---
+++ src/components/RightSidebar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="rightSidebar" v-show="sidebars.rightVisible" class="fixed bg-base-200 w-56 h-full z-50 right-0">
+    <Markdown :source="markdown" />
+  </div>
+</template>
+
+<script setup>
+import { useSidebarStore } from '@/stores/sidebar'
+import Markdown from '@/components/Markdown.vue'
+
+const sidebars = useSidebarStore()
+
+const markdown = "This is a _test_ of the **markdown** functionality including plugins."
+</script>
