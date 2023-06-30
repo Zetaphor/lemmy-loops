7f219cc73d9d55cb6e1b0dfcad2bb556f8adda84
Zetaphor <zetaphor@hey.com>
2023-06-30 17:04:14 -0300

    Parse postdata

--- src/stores/api/posts.js
+++ src/stores/api/posts.js
@@ -1,6 +1,7 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import { useApiStore } from '@/stores/api/api'
+import { DateTime } from 'luxon'
 
 export const usePostsStore = defineStore('posts', () => {
   const api = useApiStore()
@@ -8,6 +9,28 @@ export const usePostsStore = defineStore('posts', () => {
   const posts = ref([])
   // "Active" | "Hot" | "New" | "Old" | "TopDay" | "TopWeek" | "TopMonth" | "TopYear" | "TopAll" | "MostComments" | "NewComments" | "TopHour" | "TopSixHour" | "TopTwelveHour"
   const sort = ref('Hot')
+
+  function formatRelativeTime(dateString) {
+    const currentDateTime = DateTime.local()
+    const dateTime = DateTime.fromISO(dateString)
+    const diff = currentDateTime
+      .diff(dateTime)
+      .shiftTo('years', 'months', 'days', 'hours', 'minutes', 'seconds')
+
+    if (Math.abs(diff.years) >= 1) {
+      return `${Math.abs(Math.floor(diff.years))}Y`
+    } else if (Math.abs(diff.months) >= 1) {
+      return `${Math.abs(Math.floor(diff.months))}mo`
+    } else if (Math.abs(diff.days) >= 1) {
+      return `${Math.abs(Math.floor(diff.days))}d`
+    } else if (Math.abs(diff.hours) >= 1) {
+      return `${Math.abs(Math.floor(diff.hours))}h`
+    } else if (Math.abs(diff.minutes) >= 1) {
+      return `${Math.abs(Math.floor(diff.minutes))}m`
+    } else {
+      return `${Math.abs(Math.floor(diff.seconds))}s`
+    }
+  }
 
   function getFrontpage() {
     return new Promise(async (resolve, reject) => {
@@ -19,7 +42,7 @@ export const usePostsStore = defineStore('posts', () => {
 
         const postArray = postData.posts
 
-        console.log(postArray)
+        // console.log(postArray)
 
         posts.value = []
 
@@ -30,20 +53,27 @@ export const usePostsStore = defineStore('posts', () => {
             postArray[i].community.deleted ||
             postArray[i].post.removed
           ) {
-            console.log('Removed post')
-            console.log('creator_banned_from_community', postArray[i].creator_banned_from_community)
-            console.log('creator_blocked', postArray[i].creator_blocked)
-            console.log('community.deleted', postArray[i].community.deleted)
-            console.log('post.removed', postArray[i].post.removed)
+            // console.log('Removed post')
+            // console.log('creator_banned_from_community', postArray[i].creator_banned_from_community)
+            // console.log('creator_blocked', postArray[i].creator_blocked)
+            // console.log('community.deleted', postArray[i].community.deleted)
+            // console.log('post.removed', postArray[i].post.removed)
             continue
           } else {
+            const postUrl = postArray[i].post.url || ''
+            let postDomain = ''
+            if (postUrl.length)
+              postDomain = postUrl.match(/^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:/\n]+)/im)[1]
+
             posts.value.push({
               community: {
                 local: postArray[i].community.local,
                 id: postArray[i].community.id,
                 name: postArray[i].community.name,
                 title: postArray[i].community.title,
-                actor_id: postArray[i].community.actor_id
+                actor_id: postArray[i].community.actor_id,
+                actor_domain: (postArray[i].community.actor_id.match(/https:\/\/([^/]+)\/c\//) ||
+                  [])[1]
               },
               counts: {
                 comments: postArray[i].counts.comments,
@@ -56,6 +86,8 @@ export const usePostsStore = defineStore('posts', () => {
               },
               creator: {
                 actor_id: postArray[i].creator.actor_id,
+                actor_domain: (postArray[i].creator.actor_id.match(/https:\/\/([^\/]+)\/u\//) ||
+                  [])[1],
                 admin: postArray[i].creator.admin,
                 avatar: postArray[i].creator.avatar,
                 bot_account: postArray[i].creator.bot_account,
@@ -65,13 +97,15 @@ export const usePostsStore = defineStore('posts', () => {
               },
               content: {
                 ap_id: postArray[i].post.ap_id,
-                body: postArray[i].post.body,
+                body: postArray[i].post.body || '',
                 locked: postArray[i].post.locked,
                 name: postArray[i].post.name,
                 nsfw: postArray[i].post.nsfw,
-                published: postArray[i].post.published,
-                updated: postArray[i].post.updated,
-                url: postArray[i].post.url
+                published: formatRelativeTime(postArray[i].post.published),
+                updated: formatRelativeTime(postArray[i].post.updated),
+                url: postUrl,
+                url_domain: postDomain,
+                hasImage: /\.(jpg|jpeg|png|gif|bmp|webp|svg|ico)$/i.test(postArray[i].post.url)
               }
             })
           }
