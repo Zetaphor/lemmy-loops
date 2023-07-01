54266b246b9bc263d43c7071c3d2619403ed0a7d
Zetaphor <zetaphor@hey.com>
2023-06-30 22:35:39 -0300

    Set index background color

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
   <header class="fixed top-0 w-full z-10">
     <TopNav />
   </header>
-  <main class="pt-20 pb-20 bg-gray-950">
+  <main class="pt-20 pb-20">
     <RouterView />
   </main>
   <footer class="fixed bottom-0 w-full z-10">
