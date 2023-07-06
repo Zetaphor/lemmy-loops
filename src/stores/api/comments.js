435bc57fa308975befe876f7b49a5ef15c63b5e8
Zetaphor <zetaphor@hey.com>
2023-07-06 19:21:46 -0300

    Remove unused function

--- src/stores/api/comments.js
+++ src/stores/api/comments.js
@@ -84,58 +84,6 @@ export const useCommentsStore = defineStore('comments', () => {
     return newCommentArray
   }
 
-  function generateCommentTree(comments) {
-    let map = new Map()
-    comments.forEach((item) => {
-      const parsedItem = {
-        children: [],
-        comment: {
-          id: item.comment.id,
-          content: item.comment.content,
-          creator_id: item.comment.creator_id,
-          distinguished: item.comment.distinguished,
-          local: item.comment.local,
-          ap_id: item.comment.ap_id,
-          published: item.comment.published,
-          path: item.comment.path
-        },
-        counts: {
-          child_count: item.counts.child_count,
-          downvotes: item.counts.downvotes,
-          upvotes: item.counts.upvotes,
-          score: item.counts.score
-        },
-        creator: {
-          actor_id: item.creator.actor_id,
-          actor_domain: (item.creator.actor_id.match(/https:\/\/([^/]+)\/u\//) || [])[1],
-          admin: item.creator.admin,
-          avatar: item.creator.avatar,
-          bot_account: item.creator.bot_account,
-          id: item.creator.id,
-          local: item.creator.local,
-          name: item.creator.name
-        }
-      }
-
-      map.set(parsedItem.comment.id, { ...parsedItem, children: [] })
-    })
-
-    let root = { id: '0', children: [] }
-
-    map.forEach((item) => {
-      let pathArr = item.comment.path.split('.')
-      let parentPathId = pathArr[pathArr.length - 2]
-
-      if (parentPathId === '0') {
-        root.children.push(item)
-      } else if (map.has(parseInt(parentPathId))) {
-        map.get(parseInt(parentPathId)).children.push(item)
-      }
-    })
-
-    return root.children
-  }
-
   return {
     sort,
     view,
