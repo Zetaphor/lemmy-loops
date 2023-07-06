3cf2982a32620e64e20e68d745688c1be49d20ea
Zetaphor <zetaphor@hey.com>
2023-07-06 18:00:23 -0300

    Add better algorithm for parsing comment depth

--- src/stores/api/comments.js
+++ src/stores/api/comments.js
@@ -9,19 +9,82 @@ export const useCommentsStore = defineStore('comments', () => {
   const view = ref('All')
   const page = ref(1)
 
-  const renderedNodes = ref(1)
   const limit = ref(15)
 
   async function getComments(post_id) {
     return new Promise(async (resolve, reject) => {
       try {
         const resp = await api.getComments(post_id, sort.value, view.value, page.value, limit.value)
-        resolve(generateCommentTree(resp.comments))
+        resolve(generateFlatSortedCommentArray(resp.comments))
       } catch (error) {
         console.log(error)
         reject(error)
       }
     })
+  }
+
+  function generateFlatSortedCommentArray(data) {
+    let newCommentArray = data.map((commentObj) => {
+      let newCommentObj = {
+        comment: {
+          id: commentObj.comment.id,
+          content: commentObj.comment.content,
+          creator_id: commentObj.comment.creator_id,
+          distinguished: commentObj.comment.distinguished,
+          local: commentObj.comment.local,
+          ap_id: commentObj.comment.ap_id,
+          published: commentObj.comment.published,
+          path: commentObj.comment.path
+        },
+        counts: {
+          child_count: commentObj.counts.child_count,
+          downvotes: commentObj.counts.downvotes,
+          upvotes: commentObj.counts.upvotes,
+          score: commentObj.counts.score
+        },
+        creator: {
+          actor_id: commentObj.creator.actor_id,
+          actor_domain: (commentObj.creator.actor_id.match(/https:\/\/([^/]+)\/u\//) || [])[1],
+          admin: commentObj.creator.admin,
+          avatar: commentObj.creator.avatar,
+          bot_account: commentObj.creator.bot_account,
+          id: commentObj.creator.id,
+          local: commentObj.creator.local,
+          name: commentObj.creator.name
+        }
+      }
+
+      newCommentObj.comment.depth = newCommentObj.comment.path.split('.').length - 1
+
+      return newCommentObj
+    })
+
+    newCommentArray.sort((a, b) => {
+      let pathA = a.comment.path.split('.').map(Number)
+      let pathB = b.comment.path.split('.').map(Number)
+
+      for (let i = 0; i < Math.min(pathA.length, pathB.length); i++) {
+        if (pathA[i] < pathB[i]) {
+          return -1
+        } else if (pathA[i] > pathB[i]) {
+          return 1
+        }
+      }
+
+      if (pathA.length < pathB.length) {
+        return -1
+      } else if (pathA.length > pathB.length) {
+        return 1
+      } else {
+        return 0
+      }
+    })
+
+    // for (let i = 0; i < newCommentArray.length; i++) {
+    //   console.log(newCommentArray[i].comment.depth, newCommentArray[i].comment.content)
+    // }
+
+    return newCommentArray
   }
 
   function generateCommentTree(comments) {
@@ -78,7 +141,6 @@ export const useCommentsStore = defineStore('comments', () => {
 
   return {
     limit,
-    renderedNodes,
     page,
     sort,
     view,
