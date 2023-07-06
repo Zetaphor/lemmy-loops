692a9226264daaec176a724423ffe0d6ca04d00a
Zetaphor <zetaphor@hey.com>
2023-07-06 19:16:59 -0300

    Pass post ID to CommentItemList

--- src/views/CommentsView.vue
+++ src/views/CommentsView.vue
@@ -1,9 +1,11 @@
 <template>
-  <CommentItemList />
+  <CommentItemList :post_id="props.post_id" />
   <BottomNav />
 </template>
 
 <script setup>
 import CommentItemList from '@/components/comments/CommentItemList.vue'
 import BottomNav from '@/components/posts/BottomNav.vue';
+
+const props = defineProps(['post_id'])
 </script>
