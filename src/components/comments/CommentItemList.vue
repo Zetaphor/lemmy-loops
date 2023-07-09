b038d5d4313b867af5d4731ff99342715dd9fbb8
Zetaphor <zetaphor@hey.com>
2023-07-09 14:49:42 -0300

    Add comment sticky header

--- src/components/comments/CommentItemList.vue
+++ src/components/comments/CommentItemList.vue
@@ -1,7 +1,39 @@
 <template>
   <div ref="containerEl" class="w-screen h-screen relative overflow-scroll pt-2 pb-96">
-    <CommentPostData :post="postData" />
+    <div ref="postDataContainerEl">
+      <CommentPostData :post="postData" />
+    </div>
+    <!-- Post sticky title -->
+    <div v-show="stickyTitleVisible"
+      class="fixed top-14 z-10 card-bordered card-compact shadow-sm w-full pb-1 bg-gray-700 border-gray-600 border-b-2 border-l-0 border-r-0 border-t-0">
+      <p class="pl-1 pr-1 pt-2 text-sm text-gray-300 max-h-10 overflow-hidden overflow-ellipsis truncate">{{
+        postData.content.name
+      }}</p>
+      <div>
+        <div class="text-sm inline-flex items-center">
+          <p class="flex-grow-0 text-md ml-0.5">{{ postData.content.published }}</p>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" class="h-4 w-4">
+            <circle cx="5" cy="5" r="2" class="fill-current text-gray-600" />
+          </svg>
+          <p class="flex-grow-0 text-md">{{ postData.counts.score }}pts</p>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 10 10" class="h-4 w-4 mr-0.5">
+            <circle cx="5" cy="5" r="2" class="fill-current text-gray-600" />
+          </svg>
 
+
+          <img v-if="postData.community.icon.length && preferences.showPostCommunityIcon" :src="postData.community.icon"
+            class="h-4 w-4 rounded-sm mt-0.5 mr-1">
+          <p class="text-sm flex-grow-0 text-gray-300">{{ postData.community.name }}</p>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="-1 0 24 24" fill="none" stroke="currentColor" stroke-width="5"
+            stroke-linecap="round" stroke-linejoin="round" class="w-4 h-4 text-gray-400">
+            <path d="M9 18l6-6-6-6" />
+          </svg>
+          <p class="text-sm flex-grow-0 text-gray-400">{{ postData.community.actor_domain }}</p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Comment items -->
     <CommentItem :item="item" v-for="(item, index) in visibleComments" :key="index" />
     <div ref="scrollTargetEl"></div>
 
@@ -39,6 +71,7 @@
 import { ref, onMounted, onUnmounted } from 'vue'
 import CommentPostData from '@/components/comments/CommentPostData.vue'
 import CommentItem from '@/components/comments/CommentItem.vue';
+import { usePreferencesStore } from '@/stores/preferences'
 
 const props = defineProps(['postId', 'postData', 'postComments'])
 
@@ -46,9 +79,16 @@ const showLoader = ref(false)
 const showSadFace = ref(false)
 let visibleComments = ref([])
 
-let observer = null;
-const scrollTargetEl = ref(null)
+const preferences = usePreferencesStore()
+
+let commentPageObserver = null;
+let postDataObserver = null;
+
 const containerEl = ref(null)
+const scrollTargetEl = ref(null)
+const postDataContainerEl = ref(null)
+const stickyTitleVisible = ref(false)
+
 let currentPage = 0
 let commentsPerPage = 10
 
@@ -56,11 +96,11 @@ onMounted(() => {
   showLoader.value = false
   showSadFace.value = false
   renderMoreComments()
-  setupObserver()
+  setupObservers()
 })
 
-function setupObserver() {
-  observer = new IntersectionObserver(
+function setupObservers() {
+  commentPageObserver = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -75,16 +115,45 @@ function setupObserver() {
     }
   )
 
+  postDataObserver = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log('Postdata Visible')
+          stickyTitleVisible.value = false
+        } else {
+          console.log('Postdata Not Visible')
+          stickyTitleVisible.value = true
+        }
+      })
+    },
+    {
+      root: containerEl.value,
+      rootMargin: '1px',
+      threshold: 0.1,
+    }
+  )
+
   if (scrollTargetEl.value) {
-    observer.observe(scrollTargetEl.value)
+    commentPageObserver.observe(scrollTargetEl.value)
+  }
+
+  if (postDataContainerEl.value) {
+    postDataObserver.observe(postDataContainerEl.value)
   }
 }
 
 onUnmounted(() => {
-  if (observer && scrollTargetEl.value) {
-    observer.unobserve(scrollTargetEl.value)
-    observer.disconnect()
-    observer = null
+  if (commentPageObserver && scrollTargetEl.value) {
+    commentPageObserver.unobserve(scrollTargetEl.value)
+    commentPageObserver.disconnect()
+    commentPageObserver = null
+  }
+
+  if (postDataObserver && postDataContainerEl.value) {
+    postDataObserver.unobserve(postDataContainerEl.value)
+    postDataObserver.disconnect()
+    postDataObserver = null
   }
 })
 
